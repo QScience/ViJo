7bf8ced41dd37b3ec8b5a8c693292cf65240be86
seba-1511 <seba-1511@hotmail.com>
2013-10-08 15:27:03 +0200

    Implemented Username from vijo server.

--- javascript/vijo.api.js
+++ javascript/vijo.api.js
@@ -1,5 +1,5 @@
 var vijoAPI = (function() {
-	var extractKeywords, arrayIntersect, joinKeywords;
+	var extractKeywords, arrayIntersect, joinKeywords, getInnAcUsername;
 
 	/*
 	 * otherWords is an array of words that must be excluded from the counting.
@@ -68,6 +68,18 @@ var vijoAPI = (function() {
 	 	return result.join('\n');
 	 };
 
+	 getInnAcUsername = function (callback) {
+	 	var result, innAcWidgetUrl;
+	 	innAcWidgetUrl = 'http://inn.ac/innacwidget?callback=?';
+	 	jQuery.getJSON(innAcWidgetUrl, function(data) {
+	 		var username, html;
+	 		html = document.createElement('div');
+	 		html.innerHTML = data.html;
+	 		username = html.getElementsByClassName('uname')[0].innerText.trim();
+	 		callback(username);
+	 	});
+	 };
+
 	 return {
 		/*
 		 * We assume that publication is an object with:
@@ -76,71 +88,72 @@ var vijoAPI = (function() {
 		 * .authors: an array of authors
 		 */
 		 createViJo: function createViJo(publication) {
-		 	if (!isNan(publication)) {
+		 	if (!isNaN(publication)) {
 		 		publication = vijoData[publication];
 		 	}
 
-		 	var date, keywords, username;
-		 	username = 'seba-1511';
-		 	date = publication.date || new Date();
-		 	date = new Date(date).toISOString();
-		 	keywords = extractKeywords(publication.abstract);
-		 	publication.hotKeywords = keywords.slice(0, 5);
-		 	publication.keywords = keywords.slice(5, 10);
-		 	publication.allKeywords = keywords.slice(10, 15);
+		 	getInnAcUsername( function (username) {
+		 		var date, keywords;
+		 		date = publication.date || new Date();
+		 		date = new Date(date).toISOString();
+		 		keywords = extractKeywords(publication.abstract);
+		 		publication.hotKeywords = keywords.slice(0, 5);
+		 		publication.keywords = keywords.slice(5, 10);
+		 		publication.allKeywords = keywords.slice(10, 15);
 
-		 	publication.hotKeywords = joinKeywords(publication.hotKeywords);
-		 	publication.keywords = joinKeywords(publication.keywords);
-		 	publication.allKeywords = joinKeywords(publication.allKeywords);
-		 	publication.authors = publication.authors.join('\n');
+		 		publication.hotKeywords = joinKeywords(publication.hotKeywords);
+		 		publication.keywords = joinKeywords(publication.keywords);
+		 		publication.allKeywords = joinKeywords(publication.allKeywords);
+		 		publication.authors = publication.authors.join('\n');
 
-		 	jQuery.ajax({
-		 		url: 'http://vijo.inn.ac/api/virtualjournals.json?api_pass=3944BE0ACB0E69452EED49A35E0F18AC3E9667C1',
-		 		type: 'POST',
-		 		data: {
-		 			'virtualjournal':{
-		 				'belongs_to_logged_in_user': username,
-		 				'contains_authors_in_references': publication.authors,
-		 				'contains_authors_in_references_weight': 10,
-		 				'contains_keywords_important': publication.hotKeywords,
-		 				'contains_keywords_normal': publication.keywords,
-		 				'contains_keywords_supplementary': publication.allKeywords,
-		 				'created': date,
-		 				'created_by': username,
-		 				'created_by_url': window.location.hostname + Drupal.settings.basePath,
-		 				'description': publication.title,
-		 				'modified': date,
-		 				'title': publication.title,
-		 				'views': '0'
+		 		jQuery.ajax({
+		 			url: 'http://vijo.inn.ac/api/virtualjournals.json?api_pass=3944BE0ACB0E69452EED49A35E0F18AC3E9667C1',
+		 			type: 'POST',
+		 			data: {
+		 				'virtualjournal':{
+		 					'belongs_to_logged_in_user': username,
+		 					'contains_authors_in_references': publication.authors,
+		 					'contains_authors_in_references_weight': 10,
+		 					'contains_keywords_important': publication.hotKeywords,
+		 					'contains_keywords_normal': publication.keywords,
+		 					'contains_keywords_supplementary': publication.allKeywords,
+		 					'created': date,
+		 					'created_by': username,
+		 					'created_by_url': window.location.hostname + Drupal.settings.basePath,
+		 					'description': publication.title,
+		 					'modified': date,
+		 					'title': publication.title,
+		 					'views': '0',
+		 				}
+		 			},
+		 			success: function(data, textStatus, jqXHR) {
+		 				console.log(data);
 		 			}
-		 		},
-		 		success: function(data, textStatus, jqXHR) {
-		 			console.log(data);
-		 		}
+		 		});
 		 	});
-		 },
+},
 
-		 sendViJoPaper: function sendViJoPaper(publication) {
-		 	var date;
-		 	date = publication.date || new Date();
-		 	date = new Date(date).toISOString();
-		 	jQuery.ajax({
-		 		url: 'http://vijo.inn.ac/api/publications.json',
-		 		type: 'POST',
-		 		data: {
-		 			'publication':{
-		 				'title': publication.title,
-		 				'abstract': publication.abstract,
-		 				'authors': publication.authors,
-		 				'publication_date': date,
-		 				'source': window.location.hostname + Drupal.settings.basePath,
-		 			}
-		 		},
-		 		crossDomain: true,
-		 		success: function(data, textStatus, jqXHR) {
-		 			console.log(data);
-		 		}
-		 	});
-		 },
-		};
-	})();
+sendViJoPaper: function sendViJoPaper(publication) {
+	var date;
+	date = publication.date || new Date();
+	date = new Date(date).toISOString();
+	jQuery.ajax({
+		url: 'http://vijo.inn.ac/api/publications.json',
+		type: 'POST',
+		data: {
+			'publication':{
+				'title': publication.title,
+				'abstract': publication.abstract,
+				'authors': publication.authors,
+				'publication_date': date,
+				'source': window.location.hostname + Drupal.settings.basePath,
+			}
+		},
+		crossDomain: true,
+		success: function(data, textStatus, jqXHR) {
+			console.log(data);
+		}
+	});
+},
+};
+})();
